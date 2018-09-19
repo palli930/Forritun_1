1cc30c7d2a4042f431256b7b4a6325f826af1d0d
palli <palli720@gmail.com>
2018-09-19 11:38:50 +0000

    123

---
+++ python_timaverkefni/TileTraveller/p1.py
@@ -0,0 +1,89 @@
+#https://github.com/palli930/Forritun_1/tree/master/python_timaverkefni/TileTraveller
+
+x = 1 
+y = 1
+
+while x == 1 and y == 1:
+    print("You can travel: (N)orth.")
+    move = input("Direction: ")
+    if move == 'N' or move == 'n':
+        y += 1
+    else:
+        print("Not a valid direction!")
+
+while x == 1 and y == 2:
+    print("You can travel: (N)orth or (E)ast or (S)outh.")
+    move = input("Direction: ")
+    if move == "N" or move == 'n':
+        y += 1
+    elif move == "S" or move == "s":
+        y -= 1
+    elif move == "E" or move == "e":
+        x += 1
+    else:
+        print("Not a valid direction!")
+
+while x == 2 and y == 2:
+    print("You can travel: (E)ast or (S)outh.")
+    move = input("Direction: ")
+    if move == "E" or move == "e":
+        x -= 1
+    elif move == "S" or move == "s":
+        y -= 1
+    else:
+        print("Not a valid direction!")
+
+while x == 2 and y == 1:
+    print("You can travel: (N)orth.")
+    move = input("Direction: ")
+    if move == "N" or move == 'n':
+        y += 1
+    else:
+        print("Not a valid direction!")
+
+while x == 1 and y == 3:
+    print("You can travel: (S)outh or (E)ast.")
+    move = input("Direction: ")
+    if move == "S" or move == "s":
+        y -=1
+    elif move == "E" or move == "e":
+        x += 1
+    else:
+        print("Not a valid direction!")
+
+while x == 2 and y == 3:
+    print("You can travel: (E)ast or (W)est.")
+    move = input("Direction: ")
+    if move == "E" or move == "e":
+        x += 1
+    elif move =="W" or move == "w":
+        x -= 1
+    else:
+        print("Not a valid direction!")
+
+while x == 3 and y == 3:
+    print("You can travel: (S)outh or (W)est.")
+    move = input("Direction: ")
+    if move == "S" or move == "s":
+        y -= 1
+    elif move == "W" or move == "w":
+        x -= 1
+    else:
+        print("Not a valid direction!")
+
+while x == 3 and y == 2:
+    print("You can travel: (N)orth or (S)outh.")
+    move = input("Direction: ")
+    if move == "N" or move == 'n':
+        y += 1
+    elif move =="S" or move == "s":
+        y -= 1
+    else:
+        print("Not a valid direction!")
+
+if x == 3 and y == 1:
+    print("Victory!")
+    
+    
+
+
